d29df44eb184dea68d999155b5af22db28da6824
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2024-06-30 23:49:47 -0700

    sowftere modular pattern

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import config from './app/config'
+const app = express()
+const port = 3000
+
+// parsers 
+app.use(express.json())
+app.use(cors())
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+console.log('app is run')
+console.log(config.database_url)
+export default app 
